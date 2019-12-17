6fd22eefc99972214fdc3d3139250bbd7bd4afc7
Jenny Chiou <01xa14@ms.mingdao.edu.tw>
2019-12-17 20:43:03 +0800

    tendon

---
+++ assets/js/up2-introduce.js
@@ -0,0 +1,193 @@
+﻿var modal1 = document.getElementById('myModal1');
+var img1 = document.getElementById('myImg1');
+var modalImg1 = document.getElementById("img01");
+var captionText1 = document.getElementById("caption1");
+img1.onclick = function () {
+    modal1.style.display = "block";
+    modalImg1.src = "images/fix/Digitorum1.jpg";
+    captionText1.innerHTML = "&#20445;&#35703;&#24615;&#21103;&#26408;"; /*保護性副木*/
+};
+var span1 = document.getElementsByClassName("close1")[0];
+span1.onclick = function () {
+    modal1.style.display = "none";
+};
+
+var modal2 = document.getElementById('myModal2');
+var img2 = document.getElementById('myImg2');
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function () {
+    modal2.style.display = "block";
+    modalImg2.src = "images/fix/Digitorum2.gif";
+    captionText2.innerHTML = "&#21521;&#24515;&#25353;&#25705;"; /*向心按摩*/
+};
+var span2 = document.getElementsByClassName("close2")[0];
+span2.onclick = function () {
+    modal2.style.display = "none";
+};
+
+var modal3 = document.getElementById('myModal3');
+var img3 = document.getElementById('myImg3');
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function () {
+    modal3.style.display = "block";
+    modalImg3.src = "images/fix/Digitorum3.jpg";
+    captionText3.innerHTML = "&#25511;&#21046;&#24615;&#27963;&#21205;"; /*控制性活動*/
+};
+var span3 = document.getElementsByClassName("close3")[0];
+span3.onclick = function () {
+    modal3.style.display = "none";
+};
+
+var modal4 = document.getElementById('myModal4');
+var img4 = document.getElementById('myImg4');
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function () {
+    modal4.style.display = "block";
+    modalImg4.src = "images/fix/Digitorum4.jpg";
+    captionText4.innerHTML = "&#35703;&#33109;"; /*護腕*/
+};
+var span4 = document.getElementsByClassName("close4")[0];
+span4.onclick = function () {
+    modal4.style.display = "none";
+};
+
+var modal5 = document.getElementById('myModal5');
+var img5 = document.getElementById('myImg5');
+var modalImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function () {
+    modal5.style.display = "block";
+    modalImg5.src = "images/fix/Digitorum5.jpg";
+    captionText5.innerHTML = "&#32908;&#33137;&#28369;&#21205;&#36939;&#21205;"; /*肌腱滑動運動*/
+};
+var span5 = document.getElementsByClassName("close5")[0];
+span5.onclick = function () {
+    modal5.style.display = "none";
+};
+
+var modal6 = document.getElementById('myModal6');
+var img6 = document.getElementById('myImg6');
+var modalImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function () {
+    modal6.style.display = "block";
+    modalImg6.src = "images/fix/Digitorum6.jpg";
+    captionText6.innerHTML = "Blocking&#36939;&#21205;"; /*Blocking運動*/
+};
+var span6 = document.getElementsByClassName("close6")[0];
+span6.onclick = function () {
+    modal6.style.display = "none";
+};
+
+var modal7 = document.getElementById('myModal7');
+var img7 = document.getElementById('myImg7');
+var modalImg7 = document.getElementById("img07");
+var captionText7 = document.getElementById("caption7");
+img7.onclick = function () {
+    modal7.style.display = "block";
+    modalImg7.src = "images/fix/Digitorum7.jpg";
+    captionText7.innerHTML = "&#30703;&#27491;&#21103;&#26408; (Corrective splint)"; /*矯正副木 (Corrective splint)*/
+};
+var span7 = document.getElementsByClassName("close7")[0];
+span7.onclick = function () {
+    modal7.style.display = "none";
+};
+
+var modal8 = document.getElementById('myModal8');
+var img8 = document.getElementById('myImg8');
+var modalImg8 = document.getElementById("img08");
+var captionText8 = document.getElementById("caption8");
+img8.onclick = function () {
+    modal8.style.display = "block";
+    modalImg8.src = "images/fix/Digitorum8.jpg";
+    captionText8.innerHTML = "&#25569;&#21147;&#21644;&#25423;&#21147;&#28204;&#37327;"; /*握力和捏力測量*/
+};
+var span8 = document.getElementsByClassName("close8")[0];
+span8.onclick = function () {
+    modal8.style.display = "none";
+};
+
+var modal9 = document.getElementById('myModal9');
+var img9 = document.getElementById('myImg9');
+var modalImg9 = document.getElementById("img09");
+var captionText9 = document.getElementById("caption9");
+img9.onclick = function () {
+    modal9.style.display = "block";
+    modalImg9.src = "images/fix/need9.jpg";
+    captionText9.innerHTML = "&#25343;&#34907;&#29983;&#32025;&#12289;&#25569;&#26479;&#23376;"; /**/
+};
+var span9 = document.getElementsByClassName("close9")[0];
+span9.onclick = function () {
+    modal9.style.display = "none";
+};
+
+var modal10 = document.getElementById('myModal10');
+var img10 = document.getElementById('myImg10');
+var modalImg10 = document.getElementById("img10");
+var captionText10 = document.getElementById("caption10");
+img10.onclick = function () {
+    modal10.style.display = "block";
+    modalImg10.src = "images/fix/need10.gif";
+    captionText10.innerHTML = "&#36629;&#38459;&#21147;&#35347;&#32244;(1-3&#30917;)"; /**/
+};
+var span10 = document.getElementsByClassName("close10")[0];
+span10.onclick = function () {
+    modal10.style.display = "none";
+};
+
+var modal11 = document.getElementById('myModal11');
+var img11 = document.getElementById('myImg11');
+var modalImg11 = document.getElementById("img11");
+var captionText11 = document.getElementById("caption11");
+img11.onclick = function () {
+    modal11.style.display = "block";
+    modalImg11.src = "images/fix/need11.gif";
+    captionText11.innerHTML = "&#37325;&#38459;&#21147;(&#25569;&#21147;&#22120;)"; /**/
+};
+var span11 = document.getElementsByClassName("close11")[0];
+span11.onclick = function () {
+    modal11.style.display = "none";
+};
+
+var modal12 = document.getElementById('myModal12');
+var img12 = document.getElementById('myImg12');
+var modalImg12 = document.getElementById("img12");
+var captionText12 = document.getElementById("caption12");
+img12.onclick = function () {
+    modal12.style.display = "block";
+    modalImg12.src = "images/fix/need12.gif";
+    captionText12.innerHTML = "&#37782;&#34746;&#32114;"; /**/
+};
+var span12 = document.getElementsByClassName("close12")[0];
+span12.onclick = function () {
+    modal12.style.display = "none";
+};
+
+function btnweek1() {
+    $(".flexor1").show();
+    $(".flexor2").hide();
+    $(".flexor3").hide();
+    $(".flexor4").hide();
+
+}
+function btnweek2() {
+    $(".flexor2").show();
+    $(".flexor1").hide();
+    $(".flexor3").hide();
+    $(".flexor4").hide();
+}
+function btnweek3() {
+    $(".flexor3").show();
+    $(".flexor1").hide();
+    $(".flexor2").hide();
+    $(".flexor4").hide();
+}
+function btnweek4() {
+    $(".flexor4").show();
+    $(".flexor1").hide();
+    $(".flexor2").hide();
+    $(".flexor3").hide();
+}
---
+++ assets/js/up3-introduce.js
@@ -0,0 +1,196 @@
+﻿var modal1 = document.getElementById('myModal1');
+var img1 = document.getElementById('myImg1');
+var modalImg1 = document.getElementById("img01");
+var captionText1 = document.getElementById("caption1");
+img1.onclick = function () {
+    modal1.style.display = "block";
+    modalImg1.src = "images/fix/Extensor1.gif";
+    captionText1.innerHTML = "&#21521;&#24515;&#25353;&#25705;"; /*向心按摩*/
+};
+var span1 = document.getElementsByClassName("close1")[0];
+span1.onclick = function () {
+    modal1.style.display = "none";
+};
+
+var modal2 = document.getElementById('myModal2');
+var img2 = document.getElementById('myImg2');
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function () {
+    modal2.style.display = "block";
+    modalImg2.src = "";
+    captionText2.innerHTML = "&#36817;&#31471;&#25351;&#38291;&#38364;&#31680;(PIPJ)&#24398;&#26354;&#21644;&#20280;&#30452;&#30340;&#36939;&#21205;"; /*近端指間關節(PIPJ)彎曲和伸直的運動*/
+};
+var span2 = document.getElementsByClassName("close2")[0];
+span2.onclick = function () {
+    modal2.style.display = "none";
+};
+
+var modal3 = document.getElementById('myModal3');
+var img3 = document.getElementById('myImg3');
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function () {
+    modal3.style.display = "block";
+    modalImg3.src = "";
+    captionText3.innerHTML = "&#36817;&#31471;&#25351;&#38291;&#38364;&#31680;(PIPJ)&#24398;&#26354;&#21644;&#20280;&#30452;&#30340;&#36939;&#21205;"; /*近端指間關節(PIPJ)彎曲和伸直的運動*/
+};
+var span3 = document.getElementsByClassName("close3")[0];
+span3.onclick = function () {
+    modal3.style.display = "none";
+};
+
+var modal4 = document.getElementById('myModal4');
+var img4 = document.getElementById('myImg4');
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function () {
+    modal4.style.display = "block";
+    modalImg4.src = "images/fix/Extensor4.jpg";
+    captionText4.innerHTML = "&#25343;&#21462;&#34907;&#29983;&#32025;&#12289;&#26479;&#23376;"; /*拿取衛生紙、杯子*/
+};
+var span4 = document.getElementsByClassName("close4")[0];
+span4.onclick = function () {
+    modal4.style.display = "none";
+};
+
+var modal5 = document.getElementById('myModal5');
+var img5 = document.getElementById('myImg5');
+var modalImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function () {
+    modal5.style.display = "block";
+    modalImg5.src = "images/fix/Extensor5.jpg";
+    captionText5.innerHTML = "&#32908;&#33137;&#28369;&#21205;&#36939;&#21205;"; /*肌腱滑動運動*/
+};
+var span5 = document.getElementsByClassName("close5")[0];
+span5.onclick = function () {
+    modal5.style.display = "none";
+};
+
+var modal6 = document.getElementById('myModal6');
+var img6 = document.getElementById('myImg6');
+var modalImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function () {
+    modal6.style.display = "block";
+    modalImg6.src = "";
+    captionText6.innerHTML = "&#28472;&#36914;&#24335;&#38459;&#63882;&#24615;&#36939;&#21205;"; /*漸進式阻力性運動*/
+};
+var span6 = document.getElementsByClassName("close6")[0];
+span6.onclick = function () {
+    modal6.style.display = "none";
+};
+
+var modal7 = document.getElementById('myModal7');
+var img7 = document.getElementById('myImg7');
+var modalImg7 = document.getElementById("img07");
+var captionText7 = document.getElementById("caption7");
+img7.onclick = function () {
+    modal7.style.display = "block";
+    modalImg7.src = "images/fix/Extensor7.gif";
+    captionText7.innerHTML = "&#21521;&#24515;&#25353;&#25705;"; /*向心按摩*/
+};
+var span7 = document.getElementsByClassName("close7")[0];
+span7.onclick = function () {
+    modal7.style.display = "none";
+};
+
+var modal8 = document.getElementById('myModal8');
+var img8 = document.getElementById('myImg8');
+var modalImg8 = document.getElementById("img08");
+var captionText8 = document.getElementById("caption8");
+img8.onclick = function () {
+    modal8.style.display = "block";
+    modalImg8.src = "";
+    captionText8.innerHTML = "&#28145;&#22739;&#25353;&#25705;"; /*深壓按摩*/
+};
+var span8 = document.getElementsByClassName("close8")[0];
+span8.onclick = function () {
+    modal8.style.display = "none";
+};
+
+var modal9 = document.getElementById('myModal9');
+var img9 = document.getElementById('myImg9');
+var modalImg9 = document.getElementById("img09");
+var captionText9 = document.getElementById("caption9");
+img9.onclick = function () {
+    modal9.style.display = "block";
+    modalImg9.src = "";
+    captionText9.innerHTML = "&#21246;&#63994;&#25163;(Intrinsic minus)&#21644;&#24179;&#26495;&#25163;(intrinsic plus)&#30340;&#36939;&#21205;"; /*勾狀手(Intrinsic minus)和平板手(intrinsic plus)的運動*/
+
+};
+var span9 = document.getElementsByClassName("close9")[0];
+span9.onclick = function () {
+    modal9.style.display = "none";
+};
+
+var modal10 = document.getElementById('myModal10');
+var img10 = document.getElementById('myImg10');
+var modalImg10 = document.getElementById("img10");
+var captionText10 = document.getElementById("caption10");
+img10.onclick = function () {
+    modal10.style.display = "block";
+    modalImg10.src = "images/fix/Extensor10.jpg";
+    captionText10.innerHTML = "&#25484;&#25351;&#38364;&#31680;(MPJ)&#12289;&#25351;&#38291;&#38364;&#31680;(IPJ)&#38364;&#31680;"; /*掌指關節(MPJ)、指間關節(IPJ)關節*/
+};
+var span10 = document.getElementsByClassName("close10")[0];
+span10.onclick = function () {
+    modal10.style.display = "none";
+};
+
+var modal11 = document.getElementById('myModal11');
+var img11 = document.getElementById('myImg11');
+var modalImg11 = document.getElementById("img11");
+var captionText11 = document.getElementById("caption11");
+img11.onclick = function () {
+    modal11.style.display = "block";
+    modalImg11.src = "";
+    captionText11.innerHTML = "&#32908;&#33137;&#28369;&#21205;&#36939;&#21205;"; /*肌腱滑動運動*/
+};
+var span11 = document.getElementsByClassName("close11")[0];
+span11.onclick = function () {
+    modal11.style.display = "none";
+};
+
+var modal12 = document.getElementById('myModal12');
+var img12 = document.getElementById('myImg12');
+var modalImg12 = document.getElementById("img12");
+var captionText12 = document.getElementById("caption12");
+img12.onclick = function () {
+    modal12.style.display = "block";
+    modalImg12.src = "images/fix/Extensor12.jpg";
+    captionText12.innerHTML = "&#32908;&#33137;&#28369;&#21205;&#36939;&#21205;"; /*肌腱滑動運動*/
+};
+var span12 = document.getElementsByClassName("close12")[0];
+span12.onclick = function () {
+    modal12.style.display = "none";
+};
+
+var modal13 = document.getElementById('myModal13');
+var img13 = document.getElementById('myImg13');
+var modalImg13 = document.getElementById("img13");
+var captionText13 = document.getElementById("caption13");
+img13.onclick = function () {
+    modal13.style.display = "block";
+    modalImg13.src = "images/fix/Extensor13.jpg";
+    captionText13.innerHTML = "&#25343;&#21462;&#34907;&#29983;&#32025;&#25110;&#32025;&#26479;"; /*拿取衛生紙或紙杯*/
+};
+var span13 = document.getElementsByClassName("close13")[0];
+span13.onclick = function () {
+    modal13.style.display = "none";
+};
+
+var modal14 = document.getElementById('myModal14');
+var img14 = document.getElementById('myImg14');
+var modalImg14 = document.getElementById("img14");
+var captionText14 = document.getElementById("caption14");
+img14.onclick = function () {
+    modal14.style.display = "block";
+    modalImg14.src = "";
+    captionText14.innerHTML = "&#36629;&#38459;&#63882;&#27963;&#21205;"; /*輕阻力活動*/
+};
+var span14 = document.getElementsByClassName("close14")[0];
+span14.onclick = function () {
+    modal14.style.display = "none";
+};
